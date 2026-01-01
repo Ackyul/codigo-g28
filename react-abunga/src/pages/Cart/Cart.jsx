8c4f5379d6afb51db834463beaf41819441e8767
Ackyul <yoshua949237217@gmail.com>
2025-12-31 20:11:46 -0500

    31/12

--- react-abunga/src/pages/Cart/Cart.jsx
+++ react-abunga/src/pages/Cart/Cart.jsx
@@ -43,7 +43,7 @@ const Cart = () => {
                 </div>
             </header>
 
-            <main className="container mx-auto px-4 py-8 max-w-6xl flex-grow">
+            <main className="container mx-auto px-4 py-8 max-w-6xl grow">
                 <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
                     <div className="lg:col-span-2 space-y-6">
                         {cart.map((item) => (
